b62f4a9ec425de0d475962945b4a2e569d06c78c
Cody Hoffman <cody-hoffman@uiowa.edu>
2016-12-21 23:10:57 -0600

    Add files via upload
    UI needs a little work. When rotated left in the simulator for iPhone 5 the main screen stop button was getting cut off.

---
+++ PlaySoundsViewController.swift
@@ -0,0 +1,66 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Cody Hoffman on 12/20/16.
+//  Updated by Cody Hoffman on 12/21/16.
+//  Copyright © 2016 Cody Hoffman. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL: URL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {case slow = 0, fast = 1, high = 2, low = 3, echo = 4, reverb = 5}
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+        
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .high:
+            playSound(pitch: 1000)
+        case .low:
+            playSound(pitch: -1000)
+        case .echo:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.playing)
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        stopAudio()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
